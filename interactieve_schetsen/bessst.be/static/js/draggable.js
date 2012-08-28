74fa1947742f8b2f9b2e4b7aba0d1681f114b4c6
Eric Schrijver <eric@ericschrijver.nl>
2012-08-28 18:37:01 +0200

    We kunnen nu opslaan waar de logo’s zich bevinden

--- interactieve_schetsen/bessst.be/static/js/draggable.js
+++ interactieve_schetsen/bessst.be/static/js/draggable.js
@@ -1,7 +1,20 @@
+var storePositions = function() {
+    $('.logo').each(function() {
+        positions.push( $(this).offset() );
+    });
+}
+
+var retrievePositions = function() {
+    for (var i=0; i < positions.length; i++) {
+        $($('.logo')[i]).offset(positions[i]);
+    };
+}
+
 $(function () {
     $(".logo").draggable().resizable();
     $(".logo").each(function() {
         $(this).prepend('<span class="drag">&nbsp;</span>')
     })
-});
 
+    retrievePositions();
+});
