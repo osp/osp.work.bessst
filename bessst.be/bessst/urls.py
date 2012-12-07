e396c983010f87e5876f953ebc375b4ffb9af63b
Stéphanie Vilayphiou <stephanie@stdin.fr>
2012-12-07 19:24:39 +0100

    HTML5 canvas very first time

--- bessst.be/bessst/urls.py
+++ bessst.be/bessst/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     (r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^newsletter/', include('newsletter.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'(.+\.html)/$', 'django.views.generic.simple.direct_to_template'),
 )
 
 if settings.DEBUG:
