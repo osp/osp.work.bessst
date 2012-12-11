9d2c36ed562eb21b76d9a47ebb55e32e5bb20f35
Stéphanie Vilayphiou <stephanie@stdin.fr>
2012-12-11 12:23:28 +0100

    projects: generic list/detail view and a template for the project listing

--- bessst.be/bessst/urls.py
+++ bessst.be/bessst/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+from projects.models import Project
 
 from django.contrib import admin
 from bessst import settings
@@ -16,7 +17,8 @@ urlpatterns = patterns('',
     (r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^newsletter/', include('newsletter.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'(.+\.html)/$', 'django.views.generic.simple.direct_to_template'),
+    url(r'^projects/$', ListView.as_view(queryset=Project.objects.all()), name='project-list'),
+    url(r'^projects/(?P<slug>[\w-]+)/$', DetailView.as_view(model=Project), name='project-detail'),
 )
 
 if settings.DEBUG:
