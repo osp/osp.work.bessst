6b06a0049e0e18786eb6885236e75a7811c34906
Eric Schrijver <eric@ericschrijver.nl>
2012-12-13 19:04:31 +0100

    Redirect home to projects; only projects & about for now

--- bessst.be/bessst/urls.py
+++ bessst.be/bessst/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, DetailView, ListView, list_detail
+from django.views.generic.simple import redirect_to
 from projects.models import Project
 from axis.models import Axis
 
@@ -15,7 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^$', TemplateView.as_view(template_name="home.html")),
+    #(r'^$', TemplateView.as_view(template_name="home.html")),
+    (r'^$', redirect_to, {'url': '/projects/'}),
     url(r'^newsletter/', include('newsletter.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^projects/$', list_detail.object_list, {"queryset":Project.objects.all(), "extra_context": {"axis_list" : Axis.objects.all()}}),
