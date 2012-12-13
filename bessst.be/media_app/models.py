37e379008ec1747c51135b986a7ff58bf9178a7f
Stéphanie Vilayphiou <stephanie@stdin.fr>
2012-12-13 22:23:29 +0100

    description field in image models instead of caption field => was making an error, but I'm not sure why

--- bessst.be/media_app/models.py
+++ bessst.be/media_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
 class Image(models.Model):
-    caption = models.CharField(max_length=255, verbose_name=_("Caption"))
+    description = models.CharField(max_length=255, verbose_name=_("Caption"))
     image = models.ImageField(upload_to="images", max_length=100, verbose_name=_("Image file"))
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
