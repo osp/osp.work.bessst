b1107bf3a5f19f90d0c04018a791d44c08d67872
Stéphanie Vilayphiou <stephanie@stdin.fr>
2012-12-20 13:30:55 +0100

    partners + stiltebemidelaars = 1

--- bessst.be/projects/models.py
+++ bessst.be/projects/models.py
@@ -11,7 +11,6 @@ class Project(models.Model):
     title = models.CharField(max_length=255, verbose_name=_("Title"))
     slug = models.SlugField(_("Slug"), unique=True, help_text=_("Unique identifier. Allows a constant targeting of this event."))
     producers = models.ManyToManyField(People, verbose_name=_("Stiltebemidelaars"), related_name="producer_set")
-    partners = models.ManyToManyField(People, verbose_name=_("Partners"), related_name="partner_set", null=True, blank=True)
     location = models.CharField(max_length=80, verbose_name=_("Location"), null=True)
     axis = models.ForeignKey(Axis, verbose_name=_("Axis"))
     summary = models.TextField(_("Summary"))
