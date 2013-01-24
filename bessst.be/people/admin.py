83e98da96741458ee013f5dd8b4a23714894cb24
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-01-24 16:16:24 +0100

    some tidying in the people admin

--- bessst.be/people/admin.py
+++ bessst.be/people/admin.py
@@ -5,9 +5,31 @@ from wymeditor.admin import RichTextAdmin
 
 class IndividualAdmin(RichTextAdmin):
     prepopulated_fields = {'slug': ('firstname', 'prefix', 'name')}
+    fieldsets = (
+        (_('General'), {
+            'fields': (('name', 'prefix', 'firstname'), 'slug')
+        }),
+        (_('Coordinates'), {
+            'fields': (('address', 'city'), 'phone', 'email', 'link')
+        }),
+        (_('More'), {
+            'fields': ('biography',)
+        })
+    )
 
 class OrganizationAdmin(RichTextAdmin):
     prepopulated_fields = {'slug': ('prefix', 'name')}
+    fieldsets = (
+        (_('General'), {
+            'fields': (('name', 'prefix'), 'slug')
+        }),
+        (_('Coordinates'), {
+            'fields': ('contact_person', ('address', 'city'), 'phone', 'email', 'link')
+        }),
+        (_('More'), {
+            'fields': ('biography',)
+        })
+    )
 
 admin.site.register(Individual, IndividualAdmin)
 admin.site.register(Organization, OrganizationAdmin)
