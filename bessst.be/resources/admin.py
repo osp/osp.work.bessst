39d6b11badebd80b93f337d50c82aa3df8a753f9
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-01-24 17:47:15 +0100

    add a category foreignkey to resource/inspiratie

--- bessst.be/resources/admin.py
+++ bessst.be/resources/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-from .models import Resource
+from .models import Resource, ResourceCategory
 from wymeditor.admin import RichTextAdmin
 
 class ResourceAdmin(RichTextAdmin):
     pass
 
 admin.site.register(Resource, ResourceAdmin)
+admin.site.register(ResourceCategory)
 
--- bessst.be/resources/models.py
+++ bessst.be/resources/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+class ResourceCategory(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
+    def __unicode__(self):
+        return self.name
+
 class Resource(models.Model):
+    published = models.BooleanField(_("Published"), default=False)
     title = models.CharField(max_length=255, verbose_name=_("Title"))
-    summary = models.TextField(_("Summary"))
+    axis = models.ForeignKey(ResourceCategory, verbose_name=_("Category"))
+    summary = models.TextField(_("Summary"), blank=True)
     link = models.URLField(_("Link URL"))
     #preview = models.ImageField(upload_to="")
 
