7a420e3996d32009e472bcd68bb0b7e7d820fe02
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-01-24 16:01:10 +0100

    yes, the mess is gone within the people app. So now we can't add a people from a project page, as the many-to-many can call either an individual or an organization. And of course, we have a nice HTML editor for the bios

--- bessst.be/people/admin.py
+++ bessst.be/people/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from .models import Individual, Organization, People
+from wymeditor.admin import RichTextAdmin
 
-class IndividualAdmin(admin.ModelAdmin):
+class IndividualAdmin(RichTextAdmin):
     prepopulated_fields = {'slug': ('firstname', 'prefix', 'name')}
 
-class OrganizationAdmin(admin.ModelAdmin):
+class OrganizationAdmin(RichTextAdmin):
     prepopulated_fields = {'slug': ('prefix', 'name')}
 
-admin.site.register(People)
 admin.site.register(Individual, IndividualAdmin)
 admin.site.register(Organization, OrganizationAdmin)
 
