93d417098a4409cdbb7373fecd8ded4ce083d6c1
Eric Schrijver <eric@ericschrijver.nl>
2013-02-12 16:06:01 +0100

    A home on its own
    also made a function to slide between backgrounds, but did not automate it yet.
    run
    slide(axis[0])
    in the Console to try it out

--- bessst.be/axis/views.py
+++ bessst.be/axis/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext 
+
+from axis.models import Axis
+
+def home(request):
+    tpl_params = {}
+    tpl_params['object_list'] = Axis.objects.all()
+    return render_to_response("home.html", tpl_params, context_instance = RequestContext(request))
