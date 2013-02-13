bb7fef1884abcaeae41410e909ea5efc97310858
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-02-13 16:22:10 +0100

    Oops, I did it again: forgot to track the migration file

---
+++ bessst.be/axis/migrations/0005_auto__add_unique_axis_slug.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding unique constraint on 'Axis', fields ['slug']
+        db.create_unique('axis_axis', ['slug'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'Axis', fields ['slug']
+        db.delete_unique('axis_axis', ['slug'])
+
+
+    models = {
+        'axis.axis': {
+            'Meta': {'object_name': 'Axis'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'pictogram': ('django.db.models.fields.CharField', [], {'max_length': "'20'"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'media_app.image': {
+            'Meta': {'object_name': 'Image'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['axis']
