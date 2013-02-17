b348b1c3841407e131040c8cbd62274a5eadded0
Eric Schrijver <eric@ericschrijver.nl>
2013-02-17 15:53:14 +0100

    Oh wait Images should not be in the admin. At least for now, it doesn’t really offer any added usability to expose the media_app internals

--- bessst.be/media_app/admin.py
+++ bessst.be/media_app/admin.py
@@ -2,8 +2,6 @@ from django.contrib.contenttypes import generic
 from models import Image, Document
 from django.contrib import admin
 
-admin.site.register(Image)
-
 class ImageInline(generic.GenericTabularInline):
     model = Image
     extra = 0
