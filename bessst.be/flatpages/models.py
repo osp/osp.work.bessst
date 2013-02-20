f19bfdabe8a6cdd84a3e45036156cf9b9e98c0d3
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-02-20 21:55:24 +0100

    menu and title harmonized in homepage

--- bessst.be/flatpages/models.py
+++ bessst.be/flatpages/models.py
@@ -4,10 +4,16 @@ from django.contrib.contenttypes import generic
 from media_app.models import Image, Document
 
 class FlatPage(models.Model):
-    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    title = models.CharField(max_length=255, verbose_name=_("Title (NL)"))
+    title_en = models.CharField(max_length=255, verbose_name=_("Title (EN)"))
+    title_fr = models.CharField(max_length=255, verbose_name=_("Title (FR)"))
     slug = models.SlugField(_("Slug"), unique=True, help_text=_("Unique identifier. Allows a constant targeting of this page."))
-    content = models.TextField(_("Content"))
-    infobox = models.TextField(_("Info Box"), help_text=_("Content which appears in the small box on the right side of the website."), blank=True)
+    content = models.TextField(_("Content (NL)"))
+    content_en = models.TextField(_("Content (EN)"))
+    content_fr = models.TextField(_("Content (FR)"))
+    infobox = models.TextField(_("Info Box (NL)"), help_text=_("Content which appears in the small box on the right side of the website."), blank=True)
+    infobox_en = models.TextField(_("Info Box (EN)"), help_text=_("Content which appears in the small box on the right side of the website."), blank=True)
+    infobox_fr = models.TextField(_("Info Box (FR)"), help_text=_("Content which appears in the small box on the right side of the website."), blank=True)
     image_set = generic.GenericRelation(Image)
     documents = generic.GenericRelation(Document)
 
