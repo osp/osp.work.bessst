3fec86d5c57614e96422ad9866bfbb4d362bce46
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-02-21 00:18:58 +0100

    translation in resources admin

--- bessst.be/resources/admin.py
+++ bessst.be/resources/admin.py
@@ -8,6 +8,14 @@ class ResourceAdmin(RichTextAdmin):
     list_display = ('title', 'published', 'category', 'link')
     list_filter = ('published', 'category', 'link')
     inlines = (ImageInline, DocumentInline)
+    fieldsets = (
+        (_('General'), {
+            'fields': ('published', ('title', 'title_en', 'title_fr'), 'category', 'link')
+        }),
+        (_('Summary'), {
+            'fields': (('summary', 'summary_en', 'summary_fr'))
+        })
+    )
 
 admin.site.register(Resource, ResourceAdmin)
 admin.site.register(ResourceCategory)
